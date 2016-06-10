b9e970e17b0adf0d0cff288d27fcd2f1e8fc988c
Kasper Nissen <kni@it-minds.dk>
2016-06-10 13:24:01 +0200

    ui stuff

---
+++ ui/src/main/resources/static/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by kaspernissen on 10/06/2016.
+ */
+
+//$('#btn').on('click', function(){alert("Click!")});
+
+$('#btn-martin').on('click', function(){
+    $('#movie-container').removeClass('hide');
+
+});
+
+$( document ).ready(function(){
+})
