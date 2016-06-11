c2fca96e4fc7e9b3af8d11555505007da873b165
Kasper Nissen <kni@it-minds.dk>
2016-06-11 10:56:59 +0200

    actuator added to movies

--- movie-service/src/main/java/com/rpicloud/MovieServiceApplication.java
+++ movie-service/src/main/java/com/rpicloud/MovieServiceApplication.java
@@ -1,10 +1,13 @@
 package com.rpicloud;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -18,3 +21,5 @@ public class MovieServiceApplication {
 		SpringApplication.run(MovieServiceApplication.class, args);
 	}
 }
+
+
